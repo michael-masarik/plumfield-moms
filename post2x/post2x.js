532734ecbad5af5d922a12107123350b6e97e6a4
michael-masarik <masarikfamilymichael@gmail.com>
2025-02-25 14:35:20 -0600

    Started post2x

---
+++ post2x/post2x.js
@@ -0,0 +1,39 @@
+const fetch = require("node-fetch");
+import "dotenv/config";
+
+const NOTION_API_KEY = process.env.NOTION_API_KEY;
+if (!NOTION_API_KEY) {
+    throw new Error("Notion API key is not set in the environment variables.");
+}
+const DATABASE_ID = process.env.DATABASE_ID;
+if (!DATABASE_ID) {
+    throw new Error("Notion database ID is not set in the environment variables.");
+}
+
+async function getNotionPosts() {
+    const url = `https://api.notion.com/v1/databases/${DATABASE_ID}/query`;
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${NOTION_API_KEY}`,
+            "Notion-Version": "2022-06-28",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            filter: {
+                property: "Status",
+                select: {
+                    equals: "Publish"
+                }
+            }
+        })
+    });
+
+    const data = await response.json();
+    return data.results.map(page => ({
+        id: page.id,
+        content: page.properties.Name.title[0]?.text.content || "No title"
+    }));
+}
+
+getNotionPosts().then(console.log);
