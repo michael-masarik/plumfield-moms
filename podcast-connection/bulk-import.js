38a12a27d8a030f24a5fcd59161273b008c530be
michael-masarik <masarikfamilymichael@gmail.com>
2025-02-27 11:31:39 -0600

    Testing fix

--- podcast-connection/bulk-import.js
+++ podcast-connection/bulk-import.js
@@ -232,18 +232,9 @@ async function createNotionPage(episode) {
                 },
                 {
                     object: "block",
-                    type: "code",
-                    code:{
-                        caption: [],
-                        rich_text: [
-                            {
-                                type: "text",
-                                text: {
-                                    content: `super-embed: <iframe title="Podbean Player" allowtransparency="true" height="300" width="100%" style="border: none; min-width: min(100%, 430px); height:300px;" scrolling="no" data-name="pb-iframe-player" src="https://www.podbean.com/player-v2/?from=embed&i=${episodeId}&square=1&share=1&download=1&fonts=Times%20New%20Roman&skin=f6f6f6&font-color=auto&rtl=0&logo_link=episode_page&btn-skin=c73a3a&size=300" loading="lazy" allowfullscreen=""></iframe>`, // Use the function to create the player
-                                }
-                            }
-                        ],
-                        language: "html"
+                    type: "embed",
+                    embed: {
+                        url: `https://www.podbean.com/player-v2/?from=embed&i=${episodeId}&square=1&share=1&download=1&fonts=Times%20New%20Roman&skin=f6f6f6&font-color=auto&rtl=0&logo_link=episode_page&btn-skin=c73a3a&size=300`
                     }
                 },
                 ...notionBlocks // Insert parsed show notes as Notion blocks
